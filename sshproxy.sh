be7ad7b1ddba68b1cd65c4ab85c61aecf2cad893
Jason <liang.j4son@gmail.com>
2022-02-27 22:47:15 -0800

    All files lmao

---
+++ sshproxy.sh
@@ -0,0 +1,16 @@
+# Turns on ssh proxy using socks proxy. Supply ssh ip address in arguments.
+# On ^C, socks proxy turns off and ssh tunnel is off.
+
+trap 'net_off' SIGINT
+
+function net_off() {
+	networksetup -setsocksfirewallproxystate Wi-Fi off
+	exit
+}
+
+networksetup -setsocksfirewallproxystate Wi-Fi on
+ssh -CN -D 8080 $@
+net_off
+
+
+ 
